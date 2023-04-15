50d4f961cd8180bb97b7c6eff9ff24acfb38d67f
editor-taka <takachan1050150@gmail.com>
2023-04-15 22:23:27 +0900

    edit the file names and route setting based on it

--- routes/web.php
+++ routes/web.php
@@ -23,16 +23,20 @@ Route::get('/', function () {
 
 // Blog
 Route::resource("blogs", BlogController::class)
-    ->only(["index", "store", "edit", "update", "destroy"]);
+    ->only(["index", "store", "edit",
+            "update", "destroy"]);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class,
+        'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class,
+        'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class,
+        'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
