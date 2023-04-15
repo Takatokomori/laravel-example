006220c4f20e71a5a2a097115e654e0925dbc33c
editor-taka <takachan1050150@gmail.com>
2023-04-15 11:29:25 +0900

    changed the route setting. easier way

--- resources/views/blog/form.blade.php
+++ resources/views/blog/form.blade.php
@@ -1,5 +1,5 @@
 <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
-    <form method="POST" action="{{ route('blog.store') }}">
+    <form method="POST" action="{{ route('blogs.store') }}">
         @csrf
         <input
             name="title"
