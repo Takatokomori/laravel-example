142829aa116d272f9e8be277ce6e60d3b7b60d0d
editor-taka <takachan1050150@gmail.com>
2023-04-21 06:07:24 +0900

    created migration tables

---
+++ database/migrations/2023_04_20_132037_create_course_student_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course_student', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('course_id');
+            $table->unsignedInteger('student_id');
+            // 外部キー制約
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+            $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course_student');
+    }
+};
