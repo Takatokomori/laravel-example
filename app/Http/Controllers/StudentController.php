f7fbd69ed071a4b3060e19230c9c0a23d8d519d2
editor-taka <takachan1050150@gmail.com>
2023-04-25 07:22:11 +0900

    student controller with CRUD and Request

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,14 +3,69 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use App\Http\Requests\BlogRequest;
 use App\Models\Student;
 use Illuminate\View\View;
 
 class StudentController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index(): View
     {
         $students = Student::all();
         return view("students.index", compact("students"));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id): View
+    {
+        $student = Student::findOrFail($id);
+        return view("students.show", compact("student"));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id): View
+    {
+        $student = Student::findOrFail($id);
+        return view("students.edit", compact("student"));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(BlogRequest $request, string $id)
+    {
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
