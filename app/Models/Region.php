dbe736accd4bbcf79336c54ca9ce75436d448259
editor-taka <takachan1050150@gmail.com>
2023-05-15 08:35:37 -0400

    recreate region model

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Student;
 
-class Regions extends Model
+class Region extends Model
 {
     use HasFactory;
 
